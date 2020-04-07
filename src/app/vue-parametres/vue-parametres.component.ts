0d7eecc0ea87c43ced2ba7f8261106b6a2f9e358
Nathan Joubert <Joubz>
2020-04-07 17:33:37 +0200

    dynamic price calcul

--- src/app/vue-parametres/vue-parametres.component.ts
+++ src/app/vue-parametres/vue-parametres.component.ts
@@ -12,12 +12,12 @@ export class VueParametresComponent implements OnInit {
     @Input() prixTotal;
     @Input() ville = 'Err';
 
-    monument1 = new Monument(1, 'Colisée', '1 Rue du colisée', '0800', '1000', '0');
+    monument1 = new Monument(1, 'Colisée', '1 Rue du colisée', '0800', '1000', '10');
     monument2 = new Monument(1, 'Matmut', '1 Rue du matmut', '1010', '1200', '12');
     monument3 = new Monument(1, 'Piazza', '1 Rue du piazza', '1400', '1530', '0');
-    monument4 = new Monument(1, 'Panthéon', '1 Rue du Panthéon', '0800', '0950', '0');
-    monument5 = new Monument(1, 'Piazza_2 del fuego de la mañana', '1 Rue du piazza_2', '0700', '0900', '0');
-    monument6 = new Monument(0, 'Parthénon', '1 Rue du parthénon', '0800', '0950', '0');
+    monument4 = new Monument(1, 'Panthéon', '1 Rue du Panthéon', '0800', '0950', '2');
+    monument5 = new Monument(1, 'Piazza_2 del fuego de la mañana', '1 Rue du piazza_2', '0000', '0000', '0');
+    monument6 = new Monument(0, 'Parthénon', '1 Rue du parthénon', '0000', '0000', '5');
     monument7 = new Monument(1, 'Basilique St Pierre', 'Piazza San Pietro', '0000', '0000', '10');
     monument8 = new Monument(1, 'Le forum antique', 'Via della Salara Vecchia', '0000', '0000', '5', 'Colisée');
 
@@ -28,15 +28,16 @@ export class VueParametresComponent implements OnInit {
     parcoursNonVisite: Monument[] = [
         this.monument5, this.monument6, this.monument7, this.monument8
     ];
-  private price: number;
+    private price: number;
+    private tmp: number;
 
     constructor(private router: Router,
                 private calculParametresService: CalculParametresService) {
     }
 
     ngOnInit() {
-      this.prixTotal = this.calculPrixTotal();
-      this.ville = this.calculParametresService.ville;
+        this.prixTotal = this.calculPrixTotal();
+        this.ville = this.calculParametresService.ville;
     }
 
     moveTo() {
@@ -49,6 +50,7 @@ export class VueParametresComponent implements OnInit {
             this.parcoursVisite.splice(i, 1);
             console.log(this.parcoursVisite);
             console.log(this.parcoursNonVisite);
+            this.prixTotal = this.calculPrixTotal();
         } else {
             alert('Impossible de visiter le lieu pendant le séjour');
         }
@@ -60,12 +62,18 @@ export class VueParametresComponent implements OnInit {
             this.parcoursNonVisite.splice(i, 1);
             console.log(this.parcoursVisite);
             console.log(this.parcoursNonVisite);
+            this.prixTotal = this.calculPrixTotal();
         } else {
             alert('Impossible de visiter le lieu pendant le séjour');
         }
     }
 
     calculPrixTotal(): number {
-        return -1;
+        this.tmp = 0;
+        this.parcoursVisite.forEach(monument => {
+            console.log(this.tmp);
+            this.tmp += +monument.getCoutMonument();
+        });
+        return this.tmp;
     }
 }
