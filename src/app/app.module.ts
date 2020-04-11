2d0c9bb6f7cf81fe9ee49c16e42b531e697e6436
Nathan Joubert <Joubz>
2020-04-11 15:30:06 +0200

    add popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { VueAccueilComponent } from './vue-accueil/vue-accueil.component';
-import { VueParametresComponent } from './vue-parametres/vue-parametres.component';
-import { VueParcoursComponent } from './vue-parcours/vue-parcours.component';
-import { VueSauvegardesComponent } from './vue-sauvegardes/vue-sauvegardes.component';
-import { MenuComponent } from './menu/menu.component';
-import { BouttonMenuComponent } from './boutton-menu/boutton-menu.component';
-import { MonumentCardComponent } from './monument-card/monument-card.component';
-import { MonumentListComponent } from './monument-list/monument-list.component';
+import {VueAccueilComponent, DialogVueAccueilComponent} from './vue-accueil/vue-accueil.component';
+import {VueParametresComponent} from './vue-parametres/vue-parametres.component';
+import {VueParcoursComponent} from './vue-parcours/vue-parcours.component';
+import {VueSauvegardesComponent} from './vue-sauvegardes/vue-sauvegardes.component';
+import {MenuComponent} from './menu/menu.component';
+import {BouttonMenuComponent} from './boutton-menu/boutton-menu.component';
+import {MonumentCardComponent} from './monument-card/monument-card.component';
+import {MonumentListComponent} from './monument-list/monument-list.component';
 
-import { Monument } from './models/monument.model';
-
-import { CalculParametresService } from './services/calculParametresService';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import {CalculParametresService} from './services/calculParametresService';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
 
 const appRoutes: Routes = [
-  { path: 'accueil', component: VueAccueilComponent },
-  { path: 'index', component: VueAccueilComponent },
-  { path: 'parametres', component: VueParametresComponent },
-  { path: 'sauvegarde', component: VueSauvegardesComponent },
-  { path: 'parcours', component: VueParcoursComponent },
-  { path: 'menu', component: MenuComponent },
-  { path: '', component: VueAccueilComponent }
+    {path: 'accueil', component: VueAccueilComponent},
+    {path: 'index', component: VueAccueilComponent},
+    {path: 'parametres', component: VueParametresComponent},
+    {path: 'sauvegarde', component: VueSauvegardesComponent},
+    {path: 'parcours', component: VueParcoursComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: '', component: VueAccueilComponent}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    VueAccueilComponent,
-    VueParametresComponent,
-    VueParcoursComponent,
-    VueSauvegardesComponent,
-    MenuComponent,
-    BouttonMenuComponent,
-    MonumentCardComponent,
-    MonumentListComponent,
-  ],
+    declarations: [
+        AppComponent,
+        VueAccueilComponent,
+        VueParametresComponent,
+        VueParcoursComponent,
+        VueSauvegardesComponent,
+        MenuComponent,
+        BouttonMenuComponent,
+        MonumentCardComponent,
+        MonumentListComponent,
+        DialogVueAccueilComponent
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
@@ -59,11 +59,13 @@ const appRoutes: Routes = [
         MatNativeDateModule,
         MatSelectModule,
         MatCardModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatDialogModule
     ],
-  providers: [
-    CalculParametresService
-  ],
-  bootstrap: [AppComponent]
+    providers: [
+        CalculParametresService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/vue-accueil/vue-accueil.component.ts
+++ src/app/vue-accueil/vue-accueil.component.ts
@@ -1,12 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import {CalculParametresService} from '../services/calculParametresService';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 
 interface City {
     value: string;
     viewValue: string;
+}
+
+/**
+ * Dialog component
+ */
+@Component({
+    selector: 'app-dialog-vue-accueil',
+    templateUrl: 'dialog-vue-accueil.component.html',
+    styleUrls: ['./vue-accueil.component.scss']
+})
+export class DialogVueAccueilComponent {
+    constructor(
+        public dialogRef: MatDialogRef<DialogVueAccueilComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: boolean) {
+    }
+
+    onNoClick(): void {
+        this.data = false;
+        this.dialogRef.close();
+    }
+
+    onYesClick() {
+        this.data = true;
+    }
 }
 
 @Component({
@@ -31,7 +56,8 @@ export class VueAccueilComponent implements OnInit {
     constructor(
         private router: Router,
         private calculParametresService: CalculParametresService,
-        private fb: FormBuilder
+        private fb: FormBuilder,
+        public dialog: MatDialog
     ) {
         this.dateDebutCtrl = fb.control('', [Validators.required]);
         this.dateFinCtrl = fb.control('', [Validators.required]);
@@ -46,22 +72,29 @@ export class VueAccueilComponent implements OnInit {
     }
 
     /**
+     * Open the validation DIALOG
+     */
+    openDialog(): void {
+        const dialogRef = this.dialog.open(DialogVueAccueilComponent, {
+            width: '250px',
+            data: {validation: false}
+        });
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.validation();
+            }
+        });
+    }
+
+    /**
      * Submit the form
      *
-     * @param form
      */
-    onSubmit(form: NgForm) {
+    validation() {
         const ville = this.editForm.get('ville');
-
-        console.log(this.dateDebutCtrl);
-        console.log(this.editForm.get('dateDebut'));
-
         const dateDebut = this.editForm.get('dateDebut');
-
         const dateFin = this.editForm.get('dateFin');
-
         this.calculParametresService.addVille(ville, dateDebut, dateFin);
-
         this.router.navigate(['/parametres']);
     }
 
